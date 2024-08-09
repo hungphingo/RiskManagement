2e0b56e408c4dd8917f12d57629de6357cf40fd7
phi ngo <hungphi552@gmail.com>
2024-08-09 14:08:49 +0000

    Initial Commit

---
+++ app/projectdemo/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'projectdemo/test/integration/FirstJourney',
+		'projectdemo/test/integration/pages/RisksList',
+		'projectdemo/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('projectdemo') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
